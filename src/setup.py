3b6d5990f3e70451303a2a5ff859d9bf7e28093a
Che Blankenship <cheb@Ches-MacBook-Air.local>
2021-06-29 16:23:59 -0500

    add setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import py2exe
+
+option = {
+    'bundle_files':1,
+    'compressed': True
+}
+
+setup(
+    options = {'py2exe': option},
+    console=['main.py'],
+    zipfile = 'dist.zip',
+)
