dd90b4545c3c4348aba4e1c47edab9cf61e3350a
PugNorange <cnakamura336@gmail.com>
2021-07-01 05:26:35 -0500

    Add initial windows installer for tdx desktop v3

--- src/setup.py
+++ src/setup.py
@@ -1,26 +1,18 @@
 # Created on June 30th 2021
 # Reference: https://github.com/marcelotduarte/cx_Freeze/blob/main/cx_Freeze/samples/PyQt5/setup.py
-# A simple setup script to create an executable using PyQt5. This also
-# demonstrates the method for creating a Windows executable that does not have
-# an associated console.
-#
-# PyQt5app.py is a very simple type of PyQt5 application
-#
-# Run the build process by running the command 'python setup.py build'
-#
-# If everything works well you should find a subdirectory in the build
-# subdirectory that contains the files needed to run the application
 import sys
 from cx_Freeze import setup, Executable
+# Dependencies are automatically detected, but it might need fine tuning.
+# "packages": ["os"] is used as example only
+build_exe_options = {"packages": ["os"]}
+# base="Win32GUI" should be used only for Windows GUI app
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
-options = {"build_exe": {"includes": "atexit"}}
-executables = [Executable("main.py", base=base)]
 setup(
-    name="TDX Desktop",
-    version="1.0.1",
-    description="V3 TDX Desktop",
-    options=options,
-    executables=executables,
+    name = "TDX Desktop V3",
+    version = "1.0.1",
+    description = "This is TDX Desktop V3 1.0.1. Application is designed for distributing on windows.",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("TDX-Desktop-V3.py", base=base)]
 )
